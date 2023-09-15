528ec4379c0740cea9fed3545798eba1f20c36e2
Kevin Trocolli <pitok236@gmail.com>
2023-09-15 19:57:11 -0400

    createprocess: add replace_all flag

--- hooklib/createprocess.h
+++ hooklib/createprocess.h
@@ -1,24 +1,21 @@
 #pragma once
 
 #include <windows.h>
+#include <stdbool.h>
 
-HRESULT createprocess_push_hook_w(const wchar_t *name, const wchar_t *head, const wchar_t *tail);
-HRESULT createprocess_push_hook_a(const char *name, const char *head, const char *tail);
+HRESULT createprocess_push_hook_w(const wchar_t *name, const wchar_t *head, const wchar_t *tail, bool replace_all);
+HRESULT createprocess_push_hook_a(const char *name, const char *head, const char *tail, bool replace_all);
 
 struct process_hook_sym_w {
     const wchar_t *name;
-    size_t name_size;
     const wchar_t *head;
-    size_t head_size;
     const wchar_t *tail;
-    size_t tail_size;
+    bool replace_all;
 };
 
 struct process_hook_sym_a {
     const char *name;
-    size_t name_size;
     const char *head;
-    size_t head_size;
     const char *tail;
-    size_t tail_size;
+    bool replace_all;
 };
