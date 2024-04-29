eb1ec0e2615d76a73b75173031d7c99eefd545f3
Dniel97 <Dniel97@noreply.gitea.tendokyu.moe>
2024-04-29 21:34:30 +0200

    idac: updated start.bat script

--- dist/idac/start.bat
+++ dist/idac/start.bat
@@ -2,20 +2,6 @@
 
 pushd %~dp0
 
-REM set the APP_DIR to the Y drive
-set APP_DIR=Y:\SDGT
-
-REM create the APP_DIR if it doesn't exist and redirect it to the TEMP folder
-if not exist "%APP_DIR%" (
-    subst Y: %TEMP%
-    REM timeout /t 1
-    if not exist "%APP_DIR%" (
-        mkdir "%APP_DIR%"
-    )
-)
-
-echo Mounted the Y:\ drive to the %TEMP%\SDGT folder
-
 set AMDAEMON_CFG=config_common.json ^
 config_ex.json ^
 config_jp.json ^
@@ -40,11 +26,14 @@ config_seat_single_jp.json ^
 config_hook.json
 
 start "AM Daemon" /min inject -d -k idachook.dll amdaemon.exe -c %AMDAEMON_CFG%
-inject -d -k idachook.dll ..\WindowsNoEditor\GameProject.exe -culture=en launch=Cabinet ABSLOG="..\..\..\..\..\Userdata\GameProject.log" -Master -UserDir="..\..\..\Userdata" -NotInstalled -UNATTENDED
-taskkill /f /im amdaemon.exe > nul 2>&1
 
-REM unmount the APP_DIR
-subst Y: /d > nul 2>&1
+rem JP
+rem inject -d -k idachook.dll ..\WindowsNoEditor\GameProject\Binaries\Win64\GameProject-Win64-Shipping.exe -culture=ja launch=Cabinet ABSLOG="..\..\..\..\..\Userdata\GameProject.log" -Master -UserDir="..\..\..\Userdata" -NotInstalled -UNATTENDED
+
+rem EXP
+inject -d -k idachook.dll ..\WindowsNoEditor\GameProject\Binaries\Win64\GameProject-Win64-Shipping.exe -culture=en launch=Cabinet ABSLOG="..\..\..\..\..\Userdata\GameProject.log" -Master -UserDir="..\..\..\Userdata" -NotInstalled -UNATTENDED
+
+taskkill /f /im amdaemon.exe > nul 2>&1
 
 echo.
 echo Game processes have terminated
