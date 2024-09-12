96bdacfa7c598e1bb4c7e22bec6ac433ddee3660
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-09-12 12:40:57 +0200

    kemono: remove old amdaemon workaround

--- dist/kemono/start.bat
+++ dist/kemono/start.bat
@@ -2,7 +2,7 @@
 
 pushd %~dp0
 
-start "AM Daemon" /min cmd /C timeout 10 ^& inject_x64 -d -k kemonohook_x64.dll ../amdaemon.exe -f -c ../config.json"
+start "AM Daemon" /min inject_x64 -d -k kemonohook.dll ../amdaemon.exe -f -c ../config.json
 inject_x86 -d -k kemonohook_x86.dll Parade -screen-fullscreen 0 -popupwindow -screen-width 720 -screen-height 1280 -silent-crashes
 
 taskkill /f /im amdaemon.exe > nul 2>&1
