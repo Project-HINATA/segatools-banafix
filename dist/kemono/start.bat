86556ed2c87e8bb9dd3551c4a8510cc7262f4061
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-09-16 14:29:49 +0200

    kemono: Update start.bat

--- dist/kemono/start.bat
+++ dist/kemono/start.bat
@@ -2,8 +2,8 @@
 
 pushd %~dp0
 
-start "AM Daemon" /min inject_x64 -d -k kemonohook.dll ../amdaemon.exe -f -c ../config.json
-inject_x86 -d -k kemonohook_x86.dll Parade -screen-fullscreen 0 -popupwindow -screen-width 720 -screen-height 1280 -silent-crashes
+start "AM Daemon" /min inject_x64 -d -k kemonohook_x64.dll amdaemon.exe -c config.json
+inject_x86 -d -k kemonohook_x86.dll UnityApp\Parade -screen-fullscreen 0 -popupwindow -screen-width 720 -screen-height 1280 -silent-crashes
 
 taskkill /f /im amdaemon.exe > nul 2>&1
 
