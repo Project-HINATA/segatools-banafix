047733d12260d00173090959defd9e89442d5f27
GEEKi <geek_ds@foxmail.com>
2024-12-16 11:18:00 +0800

    format code

--- util/env.c
+++ util/env.c
@@ -2,8 +2,7 @@
 
 const wchar_t* get_config_path() {
     static wchar_t path[MAX_PATH];
-    if (!GetEnvironmentVariableW(L"SEGATOOLS_CONFIG_PATH", path, MAX_PATH))
-    {
+    if (!GetEnvironmentVariableW(L"SEGATOOLS_CONFIG_PATH", path, MAX_PATH)) {
         return L".\\segatools.ini";
     }
 
