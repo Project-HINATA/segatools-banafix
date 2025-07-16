03513e7b0ca96857f568c06a3eb0b95f9c8ceb1f
Dniel97 <Dniel97@noreply.gitea.tendokyu.moe>
2025-07-16 15:44:05 +0200

    add aime scan button to default segatools.ini

--- dist/idz/launch.bat
+++ dist/idz/launch.bat
@@ -6,9 +6,10 @@ start "AM Daemon" /min inject -d -k idzhook.dll amdaemon.exe -c configDHCP_Final
 
 rem Set dipsw1=0 and uncomment the ServerBox for in store battle?
 rem inject -k idzhook.dll ServerBoxD8_Nu_x64.exe
-inject -d -k idzhook.dll InitialD0_DX11_Nu.exe
+inject -d -k idzhook.dll InitialD0_DX11_Nu.exe -m
 
-taskkill /im ServerBoxD8_Nu_x64.exe > nul 2>&1
+taskkill /f /im ServerBoxD8_Nu_x64.exe > nul 2>&1
+taskkill /f /im amdaemon.exe > nul 2>&1
 
 echo.
 echo Game processes have terminated
