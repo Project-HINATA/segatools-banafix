83a9a4942979e119e9789da091a1a8dbbd66f0b2
puniru <puniru@posteo.jp>
2025-08-22 15:03:19 +0900

    idac: allow changing up and down for custom boards

--- games/idacio/di.c
+++ games/idacio/di.c
@@ -71,6 +71,8 @@ static uint8_t idac_di_view_chg;
 static uint8_t idac_di_start;
 static uint8_t idac_di_left;
 static uint8_t idac_di_right;
+static uint8_t idac_di_up;
+static uint8_t idac_di_down;
 static uint8_t idac_di_gear[6];
 static bool idac_di_use_pedals;
 static bool idac_di_reverse_brake_axis;
@@ -247,6 +249,16 @@ static HRESULT idac_di_config_apply(const struct idac_di_config *cfg)
         return E_INVALIDARG;
     }
 
+    if (cfg->up > 32) {
+        dprintf("Wheel: Invalid up button: %i\n", cfg->up);
+        return E_INVALIDARG;
+    }
+
+    if (cfg->down > 32) {
+        dprintf("Wheel: Invalid down button: %i\n", cfg->down);
+        return E_INVALIDARG;
+    }
+
     if (cfg->shift_dn > 32) {
         dprintf("Wheel: Invalid shift down button: %i\n", cfg->shift_dn);
 
@@ -282,6 +294,8 @@ static HRESULT idac_di_config_apply(const struct idac_di_config *cfg)
     dprintf("Wheel: View Change button  : %i\n", cfg->view_chg);
     dprintf("Wheel: Left button . . . . : %i\n", cfg->left);
     dprintf("Wheel: Right button  . . . : %i\n", cfg->right);
+    dprintf("Wheel: Up button . . . . . : %i\n", cfg->up);
+    dprintf("Wheel: Down button  . . .  : %i\n", cfg->down);
     dprintf("Wheel: Shift Down button . : %i\n", cfg->shift_dn);
     dprintf("Wheel: Shift Up button . . : %i\n", cfg->shift_up);
     dprintf("Wheel: Reverse Brake Axis  : %i\n", cfg->reverse_brake_axis);
@@ -317,6 +331,8 @@ static HRESULT idac_di_config_apply(const struct idac_di_config *cfg)
     idac_di_view_chg = cfg->view_chg;
     idac_di_left = cfg->left;
     idac_di_right = cfg->right;
+    idac_di_up = cfg->up;
+    idac_di_down = cfg->down;
     idac_di_shift_dn = cfg->shift_dn;
     idac_di_shift_up = cfg->shift_up;
     idac_di_reverse_brake_axis = cfg->reverse_brake_axis;
@@ -478,6 +494,14 @@ static void idac_di_get_buttons(uint8_t *gamebtn_out)
 
     if (idac_di_right && state.st.rgbButtons[idac_di_right - 1]) {
         gamebtn |= IDAC_IO_GAMEBTN_RIGHT;
+    }
+
+    if (idac_di_up && state.st.rgbButtons[idac_di_up - 1]) {
+        gamebtn |= IDAC_IO_GAMEBTN_UP;
+    }
+
+    if (idac_di_down && state.st.rgbButtons[idac_di_down - 1]) {
+        gamebtn |= IDAC_IO_GAMEBTN_DOWN;
     }
 
     *gamebtn_out = gamebtn;
